0197df41c06c924c4c26e93a392e5a94b8cd9cce
Blessed Sibanda <blessedsibanda.dev@gmail.com>
2021-09-14 06:42:08 +0200

    add hotwired comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,8 @@
 class Comment < ApplicationRecord
+  include ActionView::RecordIdentifier
   belongs_to :project
+
+  validates :body, presence: true
+
+  after_create_commit { broadcast_prepend_to [project, :comments], target: "#{dom_id(project)}_comments" }
 end
